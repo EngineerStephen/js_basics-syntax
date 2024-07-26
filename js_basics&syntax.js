58105d3e2c081d4952b9c1a8f716ededa3ceb979
Stephen Asiedu <stephen.asiedu17@gmail.com>
2024-07-26 02:46:22 -0400

    assignment completed

--- js_basics&syntax.js
+++ js_basics&syntax.js
@@ -1,4 +1,4 @@
-
+//setting variables and printing them
 let names = "Stephen, Bria, Chiamaka, Akil, Emmanuel, Nana, Darrion"
 let age ="255,30,42,35,12,14,45"
 let grades = "A+, A+, B+, B-, A, A-, A+"
@@ -9,12 +9,14 @@ console.log("Their student ages are: " + age)
 console.log("And here are their grades, respectively: " + grades) 
 console.log("And their respective attendance: " + attendance) 
 
+//declaring variables 
 let value_1;
 let value_2;
 
 value_1 = 92;
 value_2 = 112;
 
+//doing arithmetic and printing them
 let sum = value_1 + value_2;
 let difference = value_2 - value_1; 
 let product = value_1*value_2; 
@@ -26,6 +28,23 @@ console.log("The difference of the are: " +  difference);
 console.log("The product of the numbers are: " + product);
 console.log("The quotient  of the numbers are: " + quotient);
 
+//updating values an printing 
+value_1 = 500;
+value_2 = 2597; 
 
-console.log("Updated num1:", ...);
-console.log("Updated num2:", ...);
+console.log("The updated numbers are: " + value_1 + " and " + value_2 + " respectively");
+
+// Using comparison operators
+console.log("Are the numbers equal? ", value_1 === value_2 );
+console.log("Is the first number greater than the second?", value_1 > value_2);
+console.log("The first number is not equal to the second", value_1 !== value_2);
+
+
+//using logical operators 
+let isPositive = value_1 > 0 && value_2 > 0;
+let isOdd = value_1 % 2 ===1  || value_2 % 2 ===0; 
+
+
+console.log("Both are positive: ", isPositive);
+console.log("All the numbers are even", isOdd);
+
