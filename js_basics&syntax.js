ce40ffcd3f6e69bbff78be2cee9454dcf2490dae
Stephen Asiedu <stephen.asiedu17@gmail.com>
2024-07-26 01:25:33 -0400

    before task 4

---
+++ js_basics&syntax.js
@@ -0,0 +1,31 @@
+
+let names = "Stephen, Bria, Chiamaka, Akil, Emmanuel, Nana, Darrion"
+let age ="255,30,42,35,12,14,45"
+let grades = "A+, A+, B+, B-, A, A-, A+"
+let attendance = "Present, present, absent, online, present, present, absent"
+
+console.log("The student grades are: " + names) 
+console.log("Their student ages are: " + age) 
+console.log("And here are their grades, respectively: " + grades) 
+console.log("And their respective attendance: " + attendance) 
+
+let value_1;
+let value_2;
+
+value_1 = 92;
+value_2 = 112;
+
+let sum = value_1 + value_2;
+let difference = value_2 - value_1; 
+let product = value_1*value_2; 
+let quotient = value_1/value_2;
+
+console.log("Hey Stephen, of the numbers are " + value_1 + " and " + value_2)
+console.log("The sum of the numbers are:  ", + sum);
+console.log("The difference of the are: " +  difference);
+console.log("The product of the numbers are: " + product);
+console.log("The quotient  of the numbers are: " + quotient);
+
+
+console.log("Updated num1:", ...);
+console.log("Updated num2:", ...);
